976118c464e794053bae056d84a3b52365e286ed
AlaaAbdullah13 <alaaabdallah33339@gmail.com>
2026-03-07 02:44:43 +0200

    first commit

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Configuration file - Shared variables
+
+# Path to databases storage
+DATA_DIR="./databases"
+
+# Metadata suffix
+META_EXT=".meta"
+
+export DATA_DIR META_EXT
---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Main Entry Point
+
+# shellcheck source=./config.sh
+source ./config.sh
+
+echo "Welcome to Bash DBMS"
+# Here you will call the main menu function later
